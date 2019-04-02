ad1fd94618b536956c9c6e8f814eb4da9d5be86b
Quentin Meeus <qmeeus@gmail.com>
2019-04-02 15:47:05 +0200

    add dutch stopwords

--- summa/preprocessing/stopwords.py
+++ summa/preprocessing/stopwords.py
@@ -142,44 +142,50 @@ omkring overalt samme sammen selvom senere siden stadig synes syntes således te
 udover undtagen via vore vær øvrigt
 """
 
+dutch = """
+aan aangaande aangezien achte achter achterna af afgelopen al aldaar aldus alhoewel alias alle allebei alleen alles als alsnog altijd altoos ander andere anders anderszins beetje behalve behoudens beide beiden ben beneden bent bepaald betreffende bij bijna bijv binnen binnenin blijkbaar blijken boven bovenal bovendien bovengenoemd bovenstaand bovenvermeld buiten bv daar daardoor daarheen daarin daarna daarnet daarom daarop daaruit daarvanlangs dan dat de deden deed der derde derhalve dertig deze dhr die dikwijls dit doch doe doen doet door doorgaand drie duizend dus echter een eens eer eerdat eerder eerlang eerst eerste eigen eigenlijk elk elke en enig enige enigszins enkel er erdoor erg ergens etc etcetera even eveneens evenwel gauw ge gedurende geen gehad gekund geleden gelijk gemoeten gemogen genoeg geweest gewoon gewoonweg haar haarzelf had hadden hare heb hebben hebt hedden heeft heel hem hemzelf hen het hetzelfde hier hierbeneden hierboven hierin hierna hierom hij hijzelf hoe hoewel honderd hun hunne ieder iedere iedereen iemand iets ik ikzelf in inderdaad inmiddels intussen inzake is ja je jezelf jij jijzelf jou jouw jouwe juist jullie kan klaar kon konden krachtens kun kunnen kunt laatst later liever lijken lijkt maak maakt maakte maakten maar mag maken me meer meest meestal men met mevr mezelf mij mijn mijnent mijner mijzelf minder miss misschien missen mits mocht mochten moest moesten moet moeten mogen mr mrs mw na naar nadat nam namelijk nee neem negen nemen nergens net niemand niet niets niks noch nochtans nog nogal nooit nu nv of ofschoon om omdat omhoog omlaag omstreeks omtrent omver ondanks onder ondertussen ongeveer ons onszelf onze onzeker ooit ook op opnieuw opzij over overal overeind overige overigens paar pas per precies recent redelijk reeds rond rondom samen sedert sinds sindsdien slechts sommige spoedig steeds tamelijk te tegen tegenover tenzij terwijl thans tien tiende tijdens tja toch toe toen toenmaals toenmalig tot totdat tussen twee tweede u uit uitgezonderd uw vaak vaakwat van vanaf vandaan vanuit vanwege veel veeleer veertig verder verscheidene verschillende vervolgens via vier vierde vijf vijfde vijftig vol volgend volgens voor vooraf vooral vooralsnog voorbij voordat voordezen voordien voorheen voorop voorts vooruit vrij vroeg waar waarom waarschijnlijk wanneer want waren was wat we wederom weer weg wegens weinig wel weldra welk welke werd werden werder wezen whatever wie wiens wier wij wijzelf wil wilden willen word worden wordt zal ze zei zeker zelf zelfde zelfs zes zeven zich zichzelf zij zijn zijne zijzelf zo zoals zodat zodra zonder zou zouden zowat zulk zulke zullen zult"""
+
+
 italian = """
-un avete dal voi nostri avesti stiano starò sull tutto faccio sarai vostri farebbe ai degli farò c faccia lo 
-sullo farà facevate avendo fummo stiamo staranno questi sia con sue al mio fareste ero di e avessi alle avreste 
-avesse alla avrei avemmo col ad ne avremmo avevano tuo avessero siate suoi facevo ti che mi questa avrebbe fossero 
-tua starebbero faceste facesti anche cui ho tra foste stavamo non stessi avevate nostre quelli queste avrete eri 
+un avete dal voi nostri avesti stiano starò sull tutto faccio sarai vostri farebbe ai degli farò c faccia lo
+sullo farà facevate avendo fummo stiamo staranno questi sia con sue al mio fareste ero di e avessi alle avreste
+avesse alla avrei avemmo col ad ne avremmo avevano tuo avessero siate suoi facevo ti che mi questa avrebbe fossero
+tua starebbero faceste facesti anche cui ho tra foste stavamo non stessi avevate nostre quelli queste avrete eri
 facemmo stavate stia in dagl avrò avremo se feci furono io stavano nelle quante per abbiano nell faceva fecero steste
 eravamo farei sarei avevi sui quanto dai dello era loro su quello fossi stava nostra quale una farete gli siano avranno
-i stette fece negli facciano facevano dove vostra farebbero sugli vostro uno aveva dall ha avuto avuti sarete sulla sarà 
-perché essendo fai siete facendo da avevamo starà o faranno lei mie stiate nel fu facciamo stessero noi facciate stando 
-si è avute sarebbero miei sto contro avrà coi chi ci avrebbero aveste stettero abbiamo sarebbe agl del stareste sua faremo 
-siamo fanno sei abbiate fui ed quella dalle facessero tue fosti facevamo erano stessimo nei facessimo nello le dell abbia 
-fosse farai facesse starai stavo staremo mia stesse avevo lui agli fossimo dagli vostre stanno sareste quanti stemmo facessi 
-ebbe stesti tuoi dallo tutti sugl staremmo vi la dei quanta ebbero stavi saranno delle dalla saresti staresti stai suo nostro 
-aremo starete saremmo sarò li hai allo avresti dov avuta faresti starei il quelle degl all a ebbi nella eravate stetti negl 
+i stette fece negli facciano facevano dove vostra farebbero sugli vostro uno aveva dall ha avuto avuti sarete sulla sarà
+perché essendo fai siete facendo da avevamo starà o faranno lei mie stiate nel fu facciamo stessero noi facciate stando
+si è avute sarebbero miei sto contro avrà coi chi ci avrebbero aveste stettero abbiamo sarebbe agl del stareste sua faremo
+siamo fanno sei abbiate fui ed quella dalle facessero tue fosti facevamo erano stessimo nei facessimo nello le dell abbia
+fosse farai facesse starai stavo staremo mia stesse avevo lui agli fossimo dagli vostre stanno sareste quanti stemmo facessi
+ebbe stesti tuoi dallo tutti sugl staremmo vi la dei quanta ebbero stavi saranno delle dalla saresti staresti stai suo nostro
+aremo starete saremmo sarò li hai allo avresti dov avuta faresti starei il quelle degl all a ebbi nella eravate stetti negl
 come questo facevi sulle più tu della sono starebbe sul hanno faremmo sta avrai avessimo ma l
 """
 
 # stopwords from https://github.com/bieli/stopwords repository
 polish = """
-a aby ach acz aczkolwiek aj albo ale alez ależ ani az aż bardziej bardzo beda bedzie bez deda będą bede będę 
-będzie bo bowiem by byc być byl byla byli bylo byly był była było były bynajmniej cala cali caly cała cały ci 
-cie ciebie cię co cokolwiek cos coś czasami czasem czemu czy czyli daleko dla dlaczego dlatego do dobrze 
-dokad dokąd dosc dość duzo dużo dwa dwaj dwie dwoje dzis dzisiaj dziś gdy gdyby gdyz gdyż gdzie gdziekolwiek 
-gdzies gdzieś go i ich ile im inna inne inny innych iz iż ja jak jakas jakaś jakby jaki jakichs jakichś jakie 
-jakis jakiś jakiz jakiż jakkolwiek jako jakos jakoś ją je jeden jedna jednak jednakze jednakże jedno jego jej 
-jemu jesli jest jestem jeszcze jeśli jezeli jeżeli juz już kazdy każdy kiedy kilka kims kimś kto ktokolwiek 
-ktora ktore ktorego ktorej ktory ktorych ktorym ktorzy ktos ktoś która które którego której który których 
-którym którzy ku lat lecz lub ma mają mało mam mi miedzy między mimo mna mną mnie moga mogą moi moim moj 
-moja moje moze mozliwe mozna może możliwe można mój mu musi my na nad nam nami nas nasi nasz nasza nasze 
-naszego naszych natomiast natychmiast nawet nia nią nic nich nie niech niego niej niemu nigdy nim nimi niz 
-niż no o obok od około on ona one oni ono oraz oto owszem pan pana pani po pod podczas pomimo ponad poniewaz 
-ponieważ powinien powinna powinni powinno poza prawie przeciez przecież przed przede przedtem przez przy roku 
-rowniez również sam sama są sie się skad skąd soba sobą sobie sposob sposób swoje ta tak taka taki takie 
-takze także tam te tego tej ten teraz też to toba tobą tobie totez toteż totobą trzeba tu tutaj twoi twoim 
-twoj twoja twoje twój twym ty tych tylko tym u w wam wami was wasz wasza wasze we według wiele wielu więc 
-więcej wlasnie właśnie wszyscy wszystkich wszystkie wszystkim wszystko wtedy wy z za zaden zadna zadne 
+a aby ach acz aczkolwiek aj albo ale alez ależ ani az aż bardziej bardzo beda bedzie bez deda będą bede będę
+będzie bo bowiem by byc być byl byla byli bylo byly był była było były bynajmniej cala cali caly cała cały ci
+cie ciebie cię co cokolwiek cos coś czasami czasem czemu czy czyli daleko dla dlaczego dlatego do dobrze
+dokad dokąd dosc dość duzo dużo dwa dwaj dwie dwoje dzis dzisiaj dziś gdy gdyby gdyz gdyż gdzie gdziekolwiek
+gdzies gdzieś go i ich ile im inna inne inny innych iz iż ja jak jakas jakaś jakby jaki jakichs jakichś jakie
+jakis jakiś jakiz jakiż jakkolwiek jako jakos jakoś ją je jeden jedna jednak jednakze jednakże jedno jego jej
+jemu jesli jest jestem jeszcze jeśli jezeli jeżeli juz już kazdy każdy kiedy kilka kims kimś kto ktokolwiek
+ktora ktore ktorego ktorej ktory ktorych ktorym ktorzy ktos ktoś która które którego której który których
+którym którzy ku lat lecz lub ma mają mało mam mi miedzy między mimo mna mną mnie moga mogą moi moim moj
+moja moje moze mozliwe mozna może możliwe można mój mu musi my na nad nam nami nas nasi nasz nasza nasze
+naszego naszych natomiast natychmiast nawet nia nią nic nich nie niech niego niej niemu nigdy nim nimi niz
+niż no o obok od około on ona one oni ono oraz oto owszem pan pana pani po pod podczas pomimo ponad poniewaz
+ponieważ powinien powinna powinni powinno poza prawie przeciez przecież przed przede przedtem przez przy roku
+rowniez również sam sama są sie się skad skąd soba sobą sobie sposob sposób swoje ta tak taka taki takie
+takze także tam te tego tej ten teraz też to toba tobą tobie totez toteż totobą trzeba tu tutaj twoi twoim
+twoj twoja twoje twój twym ty tych tylko tym u w wam wami was wasz wasza wasze we według wiele wielu więc
+więcej wlasnie właśnie wszyscy wszystkich wszystkie wszystkim wszystko wtedy wy z za zaden zadna zadne
 zadnych zapewne zawsze ze zeby zeznowu zł znow znowu znów zostal został żaden żadna żadne żadnych że żeby
 """
+
+
 
 LANGUAGES = {
     "danish": danish,
@@ -189,7 +195,8 @@ LANGUAGES = {
     "portuguese": portuguese,
     "swedish": swedish,
     "italian": italian,
-    "polish": polish
+    "polish": polish,
+    "dutch": dutch,
 }
 
 
